04013ea1151b74c9258d943056b24b0a078d41db
Lucas Campolim <lucascampolim@protonmail.com>
2024-05-23 00:19:25 -0400

    feat: desafio poo iphone

---
+++ exercicios/desafio-poo/Iphone.java
@@ -0,0 +1,63 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página.");
+    }
+
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Agony of Defeat - Sepultura");
+
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        meuIphone.exibirPagina("www.google.com.br");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
