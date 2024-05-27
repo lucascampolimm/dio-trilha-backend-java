335f94871ddad3dfbd0f6ee9f568ee01bc387acb
Lucas Campolim <lucascampolim@protonmail.com>
2024-05-27 06:42:30 -0400

    feat: conta bancaria com construtor

---
+++ desafios-de-codigo/poo/MainContaBancaria.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class MainContaBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoInicial = scanner.nextDouble();
+        ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+        double valorDeposito = scanner.nextDouble();
+        conta.depositar(valorDeposito);
+
+        double valorSaque = scanner.nextDouble();
+        conta.sacar(valorSaque);
+
+        scanner.close();
+    }
+}
+
+class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+        System.out.println("Deposito feito.");
+        imprimirSaldo();
+    }
+
+    public void sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            System.out.println("Saque feito.");
+        } else {
+            System.out.println("Saldo insuficiente. Saque não realizado.");
+        }
+        imprimirSaldo();
+    }
+
+    private void imprimirSaldo() {
+        System.out.printf("Saldo atual: %.1f\n", this.saldo);
+    }
+}
