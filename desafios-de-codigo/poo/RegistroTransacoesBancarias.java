6f3ec69c340a00962999ec66196895bd0852e8de
Lucas Campolim <lucascampolim@protonmail.com>
2024-05-27 05:14:20 -0400

    feat: registro transacoes bancarias

---
+++ desafios-de-codigo/poo/RegistroTransacoesBancarias.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RegistroTransacoesBancarias {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<String> transacoes = new ArrayList<>();
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+            char tipoTransacao = scanner.next().toUpperCase().charAt(0);
+            double valorTransacao = scanner.nextDouble();
+
+            if (tipoTransacao == 'D') {
+                saldo += valorTransacao;
+                transacoes.add("Deposito de " + valorTransacao);
+            } else if (tipoTransacao == 'S') {
+                saldo -= valorTransacao;
+                transacoes.add("Saque de " + valorTransacao);
+            } else {
+                System.out.println("Opção inválida. Utilize D para depósito ou S para saque.");
+                i--;
+            }
+        }
+
+        System.out.printf("Saldo: %.1f\n", saldo);
+        System.out.println("Transacoes:");
+        for (int i = 0; i < transacoes.size(); i++) {
+            System.out.println((i + 1) + ". " + transacoes.get(i));
+        }
+
+        scanner.close();
+    }
+}
